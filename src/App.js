98c1b1f97a0009914d1b7e6eae2ebdbef024eab0
Esteban Mejia <estebandeveloper10@gmail.com>
2023-01-12 13:37:46 -0500

    Entorno de React preparado para el desarrollo web de Creadores de ideas

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Este es el entorno de React predeterminado para la creacion y desarrollo de la pagina web de:
+        </p>
+        <h3>Creadores de ideas</h3>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
